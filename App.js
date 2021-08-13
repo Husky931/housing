5ec85f2b95f1b0ea461403df1b756f5c27b98c5d
Gligor <pecevgligor@gmail.com>
2021-08-13 12:26:27 +0800

    set up proper nesting for the image display

--- App.js
+++ App.js
@@ -1,22 +1,49 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Yo</Text>
-      <StatusBar style="auto" />
+      <View style={styles.topRow}>
+        <ImageBackground
+          style={styles.img}
+          source={require("./front-image.jpg")}
+        >
+          {/* <View style={styles.whiteRoundArea}></View> */}
+          {/* <Text style={styles.text}>Mhm</Text> */}
+        </ImageBackground>
+      </View>
+
+      <View style={styles.bottomRow}></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    height: "100%",
+  },
+  topRow: {
+    height: "35%",
+    width: "100%",
+  },
+  img: {
+    width: "100%",
+    height: "100%",
+  },
+  whiteRoundArea: {
+    height: 10,
+    backgroundColor: "yellow",
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+  },
+  text: {
+    color: "black",
+  },
+  bottomRow: {
+    width: "100%",
+    // backgroundColor: "#f0f8ff",
+    backgroundColor: "red",
   },
 });
