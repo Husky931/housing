40f663411e43387e8cb817c6ca4ebd7bd1958628
Gligor <pecevgligor@gmail.com>
2021-08-20 18:08:28 +0800

    write sceleton ApartmentsListOverview screen

---
+++ screens/ApartmentsListOverview.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {
+  StyleSheet,
+  View,
+  ImageBackground,
+  Text,
+  Button,
+  Image,
+  Pressable,
+} from "react-native";
+import globalStyles from "../globalStylesheet/app";
+import { responsiveFontSize } from "react-native-responsive-dimensions";
+
+export default function ApartmentsListOverview() {
+  return (
+    <View>
+      <Text>I am the ApartmentsListOverview screen</Text>
+    </View>
+  );
+}
