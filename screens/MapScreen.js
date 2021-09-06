5efe7b4591be245e02f42c347af3dfb25d24a8b9
Gligor <pecevgligor@gmail.com>
2021-09-06 07:50:45 +0200

    install maps

---
+++ screens/MapScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native-elements";
+
+export default function MapScreen() {
+  return (
+    <View>
+      <Text>I am the maps</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
